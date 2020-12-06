27a475310c3431e6b7aaf601723b2509a554d358
Amith225 <amitharun3@gmail.com>
2020-12-06 12:09:58 +0530

    added capture

--- scr/renderer.py
+++ scr/renderer.py
@@ -80,6 +80,55 @@ class Camera:
         # initially rotated by 0, 0, 0
         self.oriental_rotation(0, 0, 0)
 
+    def capture(self):
+        points_cluster = []
+        faces_cluster = []
+        for obj in self.space.objects:
+            faces = []
+            point_indexes = []
+            p1i, p2i, p3i = obj.vectors[obj.faces.transpose()]
+            side1i, side2i = p1i[:, :3] - [p2i[:, :3], p3i[:, :3]]
+            normal_i = np.cross(side1i, side2i, axis=1)
+            midi = (p1i+p2i+p3i) / 3 + obj.location
+            cam_prospect_i = (midi - self.location)[:, :3]
+            forward_prospect_i = (self.forward[:3].transpose() @ cam_prospect_i)[:, 0]
+            doti = (normal_i.transpose([0, 2, 1]) @ cam_prospect_i)[:, 0]
+            z_buffer_i = np.linalg.norm(cam_prospect_i, axis=1)
+
+            fov_val = z_buffer_i * self.fov_cos
+            visible_indices = ((doti > 0) & (forward_prospect_i > fov_val) &
+                               (z_buffer_i > self.z_near) & (z_buffer_i < self.z_far)).transpose()[0]
+            visible_faces = obj.faces[visible_indices]
+            z_buffer_i = z_buffer_i[visible_indices]
+            midi = midi[visible_indices]
+
+            light_prospect_i = np.sum(self.shutter*np.array([self.light_val(light, midi)
+                                                             for light in self.space.lights]), axis=0)
+            light_prospect_i[light_prospect_i > 1] = 1
+
+            for fi in range(len(visible_faces)):
+                face = visible_faces[fi]
+                point_indexes.extend([f for f in face if f not in point_indexes])
+                faces.append([[point_indexes.index(p) + len(points_cluster) for p in face],
+                              light_prospect_i[fi][0], z_buffer_i[fi]])
+
+            points = ((self.projection_matrix @ self.camera_matrix) @ (obj.vectors[point_indexes] +
+                                                                       obj.location - self.location))
+            points *= self.space.unit / points[:, 3, np.newaxis]
+            points_cluster.extend(points)
+            faces_cluster.extend(faces)
+
+        faces_cluster = sorted(faces_cluster, key=lambda x: x[2], reverse=True)
+
+        return points_cluster, faces_cluster
+
+    def light_val(self, light, midi):
+        l_val = 1 - (np.linalg.norm((midi - light.location), axis=1) / light.lum)
+        l_val[l_val < 0] = 0
+        l_val = l_val ** self.clarity
+        l_val = l_val / np.linalg.norm(l_val)
+        return l_val
+
     def oriental_rotation(self, r, u, f):
         r, u, f = np.radians([r, u, f])
         self.forward_rotate(f), self.right_rotate(r), self.up_rotate(u)
